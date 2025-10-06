afcae5a4fef743b513bc2db972b1e3a9550b0110
MuhammadAyub24 <43295653+MuhammadAyub24@users.noreply.github.com>
2025-10-06 15:09:59 -0400

    updated image path

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
           
           <div className="flex items-center space-x-3">
             <img
-              src="/src/assets/images/food.png"
+              src="/food.png"
               alt="Logo"
               className="h-20 w-20 object-contain"
             />
